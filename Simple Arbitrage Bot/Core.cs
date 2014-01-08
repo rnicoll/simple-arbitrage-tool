1932f4696680daf5a393bd813a90ac730b009885
J Ross Nicoll <jrn@jrn.me.uk>
2014-01-08 23:46:16 +0000

    Added initial code to determine currencies with high trading volume.

--- Simple Arbitrage Bot/Core.cs
+++ Simple Arbitrage Bot/Core.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Lostics.NCryptoExchange.CoinsE;
+using Lostics.NCryptoExchange.Cryptsy;
+using Lostics.NCryptoExchange.Model;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -7,5 +11,52 @@ namespace Lostics.SimpleArbitrageBot
 {
     public class Core
     {
+        public const string COINS_E_CONFIGURATION_FILENAME = "coins_e.conf";
+        public const string CRYPTSY_CONFIGURATION_FILENAME = "cryptsy.conf";
+
+        public static void Main()
+        {
+            using (CryptsyExchange cryptsy = CryptsyExchange.GetExchange(FindCryptsyConfigurationFile()))
+            {
+                using (CoinsEExchange coinsE = CoinsEExchange.GetExchange(FindCoinsEConfigurationFile()))
+                {
+                    DoTrading(cryptsy, coinsE);
+                }
+            }
+        }
+
+        private static void DoTrading(CryptsyExchange cryptsy, CoinsEExchange coinsE)
+        {
+            const int totalCurrencies = 10;
+            IEnumerable<string> cryptsyCurrencies = MarketAnalyser.GetHighVolumeCurrencies(cryptsy, totalCurrencies).Result;
+            IEnumerable<string> coinsECurrencies = MarketAnalyser.GetHighVolumeCurrencies(coinsE, totalCurrencies).Result;
+            HashSet<string> currencies = new HashSet<string>(
+                cryptsyCurrencies.Concat(coinsECurrencies)
+            );
+
+            Console.WriteLine("High volume currencies: ");
+            foreach (string currencyCode in currencies)
+            {
+                Console.WriteLine(currencyCode);
+            }
+
+            Console.WriteLine("\nDone");
+            Console.ReadKey();
+        }
+
+        private static FileInfo FindCoinsEConfigurationFile()
+        {
+            return new FileInfo(Path.Combine(GetConfigurationDirectory().FullName, COINS_E_CONFIGURATION_FILENAME));
+        }
+
+        private static DirectoryInfo GetConfigurationDirectory()
+        {
+            return new DirectoryInfo(Directory.GetCurrentDirectory()).Parent.Parent;
+        }
+
+        private static FileInfo FindCryptsyConfigurationFile()
+        {
+            return new FileInfo(Path.Combine(GetConfigurationDirectory().FullName, CRYPTSY_CONFIGURATION_FILENAME));
+        }
     }
 }
---
+++ Simple Arbitrage Bot/MarketAnalyser.cs
@@ -0,0 +1,37 @@
+﻿using Lostics.NCryptoExchange;
+using Lostics.NCryptoExchange.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lostics.SimpleArbitrageBot
+{
+    public class MarketAnalyser
+    {
+        public static async Task<IEnumerable<string>> GetHighVolumeCurrencies<M, O>(AbstractExchange<M, O> exchange, int totalCurrencies)
+            where M : MarketId
+            where O : OrderId
+        {
+            List<Market<M>> markets = await exchange.GetMarkets();
+
+            markets = markets.Where(x => x.QuoteCurrencyCode.Equals("BTC")).ToList();
+            markets.Sort((m1, m2) => m2.Statistics.Volume24H.CompareTo(m1.Statistics.Volume24H));
+
+            HashSet<string> currencies = new HashSet<string>();
+
+            foreach (Market<M> market in markets)
+            {
+                currencies.Add(market.BaseCurrencyCode);
+
+                if (currencies.Count >= totalCurrencies)
+                {
+                    break;
+                }
+            }
+
+            return currencies;
+        }
+    }
+}
